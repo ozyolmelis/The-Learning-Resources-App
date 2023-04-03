57650cf7a079c66faa336ba900ff1c57b386ea38
ozyolmelis <ozymelis@hotmail.com>
2023-04-04 01:07:51 +0300

    ilk commit

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<script>
+export default 
+{
+    props: ['title']
+};
+</script>
+
+<style scoped>
+    header 
+    {
+        background-color: purple;
+        width: 100%;
+        height: 5rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    header h1 
+    {
+        color: white;
+    }
+
+
+</style>
